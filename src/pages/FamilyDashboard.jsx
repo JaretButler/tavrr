40ff7fd6e754b251d28e3acaf56895c2ec2f391d
Jaret Butler <jaretbutler@gmail.com>
2026-02-10 04:28:12 +0000

    File changes

--- src/pages/FamilyDashboard.jsx
+++ src/pages/FamilyDashboard.jsx
@@ -379,7 +379,7 @@ export default function FamilyDashboard() {
           <TabsContent value="dashboard">
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           {/* Left Column - Upcoming Sessions & Balance */}
-          <div className="lg:col-span-1 space-y-6">
+          <div className="lg:col-span-1 space-y-3">
             {/* Upcoming Sessions */}
             <div>
               <div className="flex items-center justify-between mb-3">
